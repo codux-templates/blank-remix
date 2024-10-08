a8e4adedb91538fbce8b6a9349e8a6c5f2f76309
Noam Musba <noammu@wix.com>
2024-10-08 18:08:09 +0300

    feat: add error boundaries This is a better error page handling than before, as the former solution didn't work on stage and froze it

--- app/routes/error/route.tsx
+++ app/routes/error/route.tsx
@@ -1,4 +1,5 @@
 import { useSearchParams } from '@remix-run/react';
+import { ErrorComponent } from '~/components/error-component/error-component';
 
 export default function ErrorPage() {
     const [searchParams] = useSearchParams();
@@ -6,5 +7,5 @@ export default function ErrorPage() {
     const title = searchParams.get('title');
     const message = searchParams.get('message');
 
-    return <div>Error View</div>;
+    return <ErrorComponent title={title} message={message} />;
 }
