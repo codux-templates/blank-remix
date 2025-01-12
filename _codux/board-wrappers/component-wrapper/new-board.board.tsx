361579bb75cb19a15391c9264af79667ba5af51c
dorza <dorza@wix.com>
2025-01-12 12:28:14 +0200

    fix: update boards templates to work as expected

--- _codux/board-wrappers/component-wrapper/new-board.board.tsx
+++ _codux/board-wrappers/component-wrapper/new-board.board.tsx
@@ -1,18 +1,28 @@
 import { PropsWithChildren } from 'react';
 import { createRemixStub } from '@remix-run/testing';
 import { ROUTES } from '~/router/config';
+import { ContentSlot, createBoard } from '@wixc3/react-board';
 
 export interface ComponentWrapperProps extends PropsWithChildren {
     loaderData?: Record<string, unknown>;
 }
 
-export default function ComponentWrapper({ children, loaderData }: ComponentWrapperProps) {
+export function ComponentWrapper({ children, loaderData }: ComponentWrapperProps) {
     const RemixStub = createRemixStub([
         {
             Component: () => children,
-            children: [...Object.values(ROUTES).map(({ path }) => ({ path }))],
+            children: Object.values(ROUTES).map(({ path }) => ({ path }))
         },
     ]);
 
     return <RemixStub hydrationData={{ loaderData }} />;
 }
+
+export default createBoard({
+    name: 'NewBoard',
+    Board: () => (
+        <ComponentWrapper>
+            <ContentSlot />
+        </ComponentWrapper>
+    ),
+});
